4359029f42571d2720925209c238107ec778a6bb
jgusta <jgustafx@gmail.com>
2026-08-15 07:00:41 -0700

    Keep bottle setup separate from formula rebuild

--- Formula/cj.rb
+++ Formula/cj.rb
@@ -4,7 +4,6 @@ class Cj < Formula
   url "https://github.com/jgusta/cjtaskrunner/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "8f12d9acb31c89c07f927f123eba490ebf903bb701fb4de18c802b9cd8229ee6"
   license "MIT"
-  revision 1
   head "https://github.com/jgusta/cjtaskrunner.git", branch: "main"
 
   depends_on "rust" => :build
